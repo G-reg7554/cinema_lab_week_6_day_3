241ae6af6a4f5aeb07762870e4500dea652d4f6c
Gregor Campbell <gregorc93@hotmail.co.uk>
2022-08-31 20:52:36 +0100

    Adding cinema lab to github

---
+++ lab_callbacks_films_start/models/cinema.js
@@ -0,0 +1,36 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.getFilmTitles = function(){
+  
+  const filmTitles = this.films.map((film)=> {
+    return film.title
+  })
+  return filmTitles
+  
+}
+
+
+
+Cinema.prototype.getFilmTitles = function(){
+  
+  const filmTitles = this.films.map((film)=> {
+    return film.title
+  })
+  return filmTitles
+  
+}
+
+
+
+
+
+
+
+
+
+
+module.exports = Cinema;
+
+
